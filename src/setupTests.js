cb419488358f1a7b0fab09ac62863dc148a8d029
kacperkapusciak <kapusciak.k@gmail.com>
2020-05-08 20:44:11 +0200

    Add unit tests to Button component

---
+++ src/setupTests.js
@@ -0,0 +1,17 @@
+import Enzyme, { mount, shallow } from 'enzyme'
+import { ThemeConsumer } from 'styled-components'
+import Adapter from 'enzyme-adapter-react-16';
+
+import theme from './styles/theme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+export const shallowWithTheme = (children) => {
+  ThemeConsumer._currentValue = theme;
+  return shallow(children)
+};
+
+export const mountWithTheme = (children) => {
+  ThemeConsumer._currentValue = theme;
+  return mount(children)
+};
