734ae28427d719315d56918cac8dd0493a47c2ef
kacperkapusciak <kapusciak.k@gmail.com>
2020-05-09 16:34:42 +0200

    Change Settings->LeftPanel, update to use two PIN numbers

--- src/pages/Poll/LeftPanel.jsx
+++ src/pages/Poll/LeftPanel.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Field } from 'formik';
 import styled from "styled-components";
+
+import axios from 'axios-instance';
 
 import Label from 'components/Label';
 import Checkbox from 'components/Checkbox';
@@ -8,6 +10,8 @@ import Select from 'components/Select';
 import DeletePoll from 'modals/DeletePoll';
 
 import ErrorIcon from 'assets/ErrorIcon.svg';
+import UserPINIcon from 'assets/UserPINIcon.svg';
+import AdminPINIcon from 'assets/AdminPINIcon.svg';
 
 import { withModal } from 'providers/ModalProvider';
 
@@ -19,10 +23,11 @@ const Header = styled.h5`
   text-transform: uppercase;
   color: ${({ theme }) => theme.colors.neutral[700]};
   margin-bottom: 8px;
+  margin-top: ${({ margin }) => margin ? '16px' : '0'};
 `;
 const LabelStyled = styled(Label)`
   margin-top: 16px;
-  margin-bottom: 6px;
+  margin-bottom: ${({ margin }) => margin ? '6px' : 0};
   &:first-of-type {
     margin-top: 8px;
   }
@@ -52,10 +57,34 @@ const Icon = styled.img`
   margin-right: 8px;
   margin-bottom: 4px;
 `;
+const PINComponent = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+  letter-spacing: -0.7px;
+  
+  & > img {
+    height: 15px;
+    margin-right: 6px;
+  }
+`;
 
 
-function Settings(props) {
+function LeftPanel(props) {
   const { modal } = props;
+  const [PIN, setPIN] = useState({ user: null, creator: null });
+
+  useEffect(() => {
+    async function getPIN() {
+      const { data } = await axios.get('init');
+      if (data) {
+        const { pinUser, pinCreator } = data;
+        setPIN({ user: pinUser, creator: pinCreator });
+      }
+    }
+
+    getPIN();
+  }, []);
 
   const resultsAccess = [
     { value: 'me', label: 'Tylko ja' },
@@ -93,38 +122,53 @@ function Settings(props) {
 
   return (
     <div>
-      <Header>opcje</Header>
-      <LabelStyled>anonimowość</LabelStyled>
-      <AlignWrapper>
-        <Field name="settings.requireSignature">
-          {({ field }) => <Checkbox field={field}/>}
-        </Field>
-        <Info>Wymagaj podpisu</Info>
-      </AlignWrapper>
-      <LabelStyled>wgląd w odpowiedzi</LabelStyled>
-      <Field
-        name="settings.resultsAccess"
-        component={Select}
-        options={resultsAccess}
-      />
-      <LabelStyled>ankieta wygaśnie</LabelStyled>
-      <Field
-        name="settings.expire"
-        component={Select}
-        options={expire}
-      />
-      <LabelStyled>strefa zagrożenia</LabelStyled>
-      <AlignWrapper>
-        <Icon src={ErrorIcon} alt="Error icon"/>
-        <DeletePollButton
-          type="button"
-          onClick={() => modal.open(<DeletePoll/>)}
-        >
-          Usuń ankietę
-        </DeletePollButton>
-      </AlignWrapper>
+      <section>
+        <Header>bezpieczeństwo</Header>
+        <LabelStyled>pin administratora</LabelStyled>
+        <PINComponent>
+          <img src={AdminPINIcon} alt=''/>
+          {PIN.creator}
+        </PINComponent>
+        <LabelStyled>pin odpowiadającego</LabelStyled>
+        <PINComponent>
+          <img src={UserPINIcon} alt=''/>
+          {PIN.user}
+        </PINComponent>
+      </section>
+      <section>
+        <Header margin>opcje</Header>
+        <LabelStyled margin>anonimowość</LabelStyled>
+        <AlignWrapper>
+          <Field name="settings.requireSignature">
+            {({ field }) => <Checkbox field={field}/>}
+          </Field>
+          <Info>Wymagaj podpisu</Info>
+        </AlignWrapper>
+        <LabelStyled margin>wgląd w odpowiedzi</LabelStyled>
+        <Field
+          name="settings.resultsAccess"
+          component={Select}
+          options={resultsAccess}
+        />
+        <LabelStyled margin>ankieta wygaśnie</LabelStyled>
+        <Field
+          name="settings.expire"
+          component={Select}
+          options={expire}
+        />
+        <LabelStyled margin>strefa zagrożenia</LabelStyled>
+        <AlignWrapper>
+          <Icon src={ErrorIcon} alt="Error icon"/>
+          <DeletePollButton
+            type="button"
+            onClick={() => modal.open(<DeletePoll/>)}
+          >
+            Usuń ankietę
+          </DeletePollButton>
+        </AlignWrapper>
+      </section>
     </div>
   );
 };
 
-export default withModal(Settings);
+export default withModal(LeftPanel);
