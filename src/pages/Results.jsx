0592c7c6b4fa9008957e39faf550c7c5732b2329
PatrykRadon <radon.patryk@gmail.com>
2020-05-16 16:39:48 +0200

    Cards stylized, end point cleaned up

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -2,26 +2,43 @@ import React from 'react';
 import Card from "../components/QuestionCard";
 import {Bar} from "react-chartjs-2";
 import styled from "styled-components";
+import theme from "../styles/theme";
 
 const TextHeader = styled.p`
   font-size: 18px;
 `;
 const TextAnswer = styled.p`
-margin-top: 20px;
+  font-size: 14px;
+  margin-bottom: 24px;
+`;
+const TextFooter = styled.p`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.neutral[500]};
+  margin-bottom: 24px;
 `;
 
 function Results(props) {
   const { results } = props
+
   return (
     <>
     {results.map((result, rIndex) => ( (result.type !== "text") ? (
           <Card key={rIndex}>
+            <TextHeader>{result.title}</TextHeader>
+            <TextFooter>{result.total} odpowiedzi</TextFooter>
             <Bar
-              data={result.data}
+              data={{
+                  labels: result.labels,
+                  datasets: [
+                    {
+                      backgroundColor: theme.colors.primary[400],
+                      data: result.data
+                    }
+                  ]
+              }}
               options={{
                 title:{
                   display:true,
-                  text:`${result.title}`,
                   fontSize:20
                 },
                 scales: {
@@ -47,7 +64,8 @@ function Results(props) {
           </Card>
         ):(<Card key={rIndex}>
           <TextHeader>{result.title}</TextHeader>
-          {result.data.answers.map((answer, aIndex) =>
+          <TextFooter>{result.total} odpowiedzi</TextFooter>
+          {result.data.map((answer, aIndex) =>
             <TextAnswer>{answer}</TextAnswer>
           )}
         </Card>))
