af758d8801078b3d4055c6ca775b8fb7b90a05c5
kacperkapusciak <kapusciak.k@gmail.com>
2020-05-22 20:20:24 +0200

    Updates

---
+++ precache-manifest.ec0cc7dc3a8c5d81f7c3b186f6e72ddd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69423b0a186b2e54018a386bf929b000",
+    "url": "/apollo-fe/index.html"
+  },
+  {
+    "revision": "2867c145e63e0f7a0509",
+    "url": "/apollo-fe/static/js/0.6d1fcad0.chunk.js"
+  },
+  {
+    "revision": "71bb5ac22bb7acfbb354",
+    "url": "/apollo-fe/static/js/1.34b17f67.chunk.js"
+  },
+  {
+    "revision": "1fd0d3303d3b2766defc9139ddaa7f40",
+    "url": "/apollo-fe/static/js/1.34b17f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28db8ce9ddb973cde266",
+    "url": "/apollo-fe/static/js/10.d9ea9f2b.chunk.js"
+  },
+  {
+    "revision": "4777fac7e7916067ea19",
+    "url": "/apollo-fe/static/js/4.121617a6.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/apollo-fe/static/js/4.121617a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb66e3b211f8aba1dbf",
+    "url": "/apollo-fe/static/js/5.8b2473d1.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/apollo-fe/static/js/5.8b2473d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72feece2dbbb22d24c07",
+    "url": "/apollo-fe/static/js/6.e07ede00.chunk.js"
+  },
+  {
+    "revision": "9fb0539ca22a19406b85",
+    "url": "/apollo-fe/static/js/7.90d5f277.chunk.js"
+  },
+  {
+    "revision": "9af195406c3d674950d3",
+    "url": "/apollo-fe/static/js/8.c6edd04d.chunk.js"
+  },
+  {
+    "revision": "62a288e81e0d10bfbf0c",
+    "url": "/apollo-fe/static/js/9.71b5ff8c.chunk.js"
+  },
+  {
+    "revision": "bc3debb9770b5a0ec133",
+    "url": "/apollo-fe/static/js/main.6255f2dc.chunk.js"
+  },
+  {
+    "revision": "fa5578edcdc248c76d16",
+    "url": "/apollo-fe/static/js/runtime-main.aca3fdfa.js"
+  },
+  {
+    "revision": "73067c189d5ff1e6d93dc6f8b27cc9c9",
+    "url": "/apollo-fe/static/media/AdminPINIcon.73067c18.svg"
+  },
+  {
+    "revision": "453cfd08916eba91c7c3048322a191d7",
+    "url": "/apollo-fe/static/media/CrossIcon.453cfd08.svg"
+  },
+  {
+    "revision": "12ffc0ee29fa157715dfd29985ee6018",
+    "url": "/apollo-fe/static/media/ErrorIcon.12ffc0ee.svg"
+  },
+  {
+    "revision": "78dd96e05d3a384b04bd2b299ab570bd",
+    "url": "/apollo-fe/static/media/KeyIconHorizontal.78dd96e0.svg"
+  },
+  {
+    "revision": "171c75b3e0548c4f854a9c9579488a0b",
+    "url": "/apollo-fe/static/media/LandingPageBlob.171c75b3.svg"
+  },
+  {
+    "revision": "86e86e402c8003e5580fa7c1716454b1",
+    "url": "/apollo-fe/static/media/SharePollIcon.86e86e40.svg"
+  },
+  {
+    "revision": "701fccaf3babd0af1777590c518fbb0b",
+    "url": "/apollo-fe/static/media/StarOffIcon.701fccaf.svg"
+  },
+  {
+    "revision": "40e8f0df877d755bf21fa59a7bbe5c71",
+    "url": "/apollo-fe/static/media/StarOnIcon.40e8f0df.svg"
+  },
+  {
+    "revision": "824eb61b34e0b26b58458f540038d662",
+    "url": "/apollo-fe/static/media/UserPINIcon.824eb61b.svg"
+  },
+  {
+    "revision": "4c56a700edd60d799aae81978edecac8",
+    "url": "/apollo-fe/static/media/history.4c56a700.svg"
+  }
+]);
